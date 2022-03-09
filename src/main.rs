1b9d59cdb058410cdeeb30b8103472a56d044af3
Bailey Spraggins <baileyspraggins@Baileys-MacBook-Pro.local>
2022-03-09 17:19:55 -0600

    Reading files with modularity and error handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::{env, process};
+
+use minigrep::Config;
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", &config.query);
+    println!("In file {}", &config.filename);
+
+    // Use an if let funciton when running the config because we dont need to return a () value.
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    };
 }
